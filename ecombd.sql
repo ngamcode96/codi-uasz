f69a2a108c07f671d52ee6b60c138399328c33ad
Amadou NGAM <96473545+ngamcode96@users.noreply.github.com>
2022-01-17 21:29:26 +0100

    Update ecombd.sql

--- ecombd.sql
+++ ecombd.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `ngamc1600974`
+-- Database: `nom_base_de_donnees`
 --
 
 -- --------------------------------------------------------
